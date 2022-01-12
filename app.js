7dfc00ce0f69d0546fe631268e561a7df0f1af8c
Trajan Miller <trajanmiller324@gmail.com>
2022-01-12 18:31:49 -0500

    playRound function plays one round and determines the winner

--- app.js
+++ app.js
@@ -16,3 +16,26 @@ const computerPlay = (choice) => {
   }
   return choice;
 };
+
+const playRound = (playerSelection, computerSelection) => {
+  computerSelection = computerPlay();
+  console.log(`computer: ${computerSelection}`);
+
+  if (playerSelection === computerSelection) {
+    console.log("It's a tie");
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    console.log("You lose. Paper beats rock");
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    console.log("You win. Rock beats scissors");
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    console.log("You win. Paper beats rock");
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    console.log("You lose. Scissors beats paper");
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    console.log("You lose. Rock beats scissors");
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    console.log("You win. Scissors beats paper");
+  } else {
+    console.log("Error");
+  }
+};
