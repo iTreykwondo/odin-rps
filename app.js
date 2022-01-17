7c0e48ada9088cbda8beac478bb9042ab8c9a2d0
Trajan Miller <trajanmiller324@gmail.com>
2022-01-17 17:26:46 -0500

    Player score and cpu score update each round,. Once a winner is declared, the selection buttons are hidden

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const results = document.querySelector(".results h2");
 let playerScoreLabel = document.querySelector("#p-score");
 let cpuScoreLabel = document.querySelector("#cpu-score");
 
+let cpuScore = 0;
+let playerScore = 0;
+
 const computerPlay = (choice) => {
   let option = Math.floor(Math.random() * 3 + 1);
 
@@ -25,37 +28,77 @@ const computerPlay = (choice) => {
 const playRound = (playerSelection, computerSelection) => {
   // playerSelection = prompt("Enter Rock, Paper or Scissors!").toLowerCase();
   computerSelection = computerPlay();
-  console.log(`computer: ${computerSelection}`);
 
   if (playerSelection === computerSelection) {
     return (results.textContent = "It's a tie");
   } else if (playerSelection === "rock" && computerSelection === "paper") {
-    return (results.textContent = "You lose. Paper beats rock");
+    return (
+      (results.textContent = `You lose. Paper beats rock`),
+      (cpuScore += 1),
+      (cpuScoreLabel.textContent = cpuScore)
+    );
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    return (results.textContent = "You win. Rock beats scissors");
+    return (
+      (results.textContent = `You win. Rock beats scissor`),
+      (playerScore += 1),
+      (playerScoreLabel.textContent = playerScore)
+    );
   } else if (playerSelection === "paper" && computerSelection === "rock") {
-    return (results.textContent = "You win. Paper beats rock");
+    return (
+      (results.textContent = `You win. Paper beats rock`),
+      (playerScore += 1),
+      (playerScoreLabel.textContent = playerScore)
+    );
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    return (results.textContent = "You lose. Scissors beats paper");
+    return (
+      (results.textContent = `You lose. Scissors beats paper`),
+      (cpuScore += 1),
+      (cpuScoreLabel.textContent = cpuScore)
+    );
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    return (results.textContent = "You lose. Rock beats scissors");
+    return (
+      (results.textContent = `You lose. Rock beats scissors`),
+      (cpuScore += 1),
+      (cpuScoreLabel.textContent = cpuScore)
+    );
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return (results.textContent = "You win. Scissors beats paper");
+    return (
+      (results.textContent = `You win. Scissors beats paper`),
+      (playerScore += 1),
+      (playerScoreLabel.textContent = playerScore)
+    );
   } else {
     return (results.textContent = "Error");
   }
 };
 
-const scoreTracker = () => {};
+const scoreTracker = () => {
+  if (playerScore === 5) {
+    disableSelectionButtons();
+    return (results.textContent = "Player wins!");
+  } else if (cpuScore === 5) {
+    disableSelectionButtons();
+    return (results.textContent = "Computer wins!");
+  }
+};
+
+const disableSelectionButtons = () => {
+  rockButton.style.display = "none";
+  paperButton.style.display = "none";
+  scissorsButton.style.display = "none";
+};
 
 rockButton.addEventListener("click", () => {
   console.log(playRound("rock"));
+  scoreTracker();
 });
 
 paperButton.addEventListener("click", () => {
   console.log(playRound("paper"));
+  scoreTracker();
 });
 
 scissorsButton.addEventListener("click", () => {
   console.log(playRound("scissors"));
+  scoreTracker();
 });
